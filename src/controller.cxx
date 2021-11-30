c7eb133c66a5dace239fe3469bbbc12bf07b684e
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2021-11-29 23:59:14 -0600

    Added to on_key_up
    on_mouse_down not done

--- src/controller.cxx
+++ src/controller.cxx
@@ -1,12 +1,10 @@
 #include "controller.hxx"
-#include "model.hxx"
 
-Controller::Controller(Model& model)
-        : model_(model),
-        view_(model)
-        //not recognizing as member variables
+Controller::Controller(ge211::Dims<int> screen_dimensions)
+        : model_(Model Model(screen_dimensions)),
+          view_(model_),
+          player_input("")
 { }
-    //issue
 
 void
 Controller::draw(ge211::Sprite_set& set)
@@ -17,75 +15,37 @@ Controller::draw(ge211::Sprite_set& set)
 void
 Controller::on_frame(double dt)
 {
-    // Abstract_game::on_frame(dt);
     model_.on_frame(dt);
-    if (model_.on_frame(dt)) {
-        load_num_();
-        //start telling view to load the numbers that player types
-        //asteroids
-    }
 }
 
+//For restart button
+void
+Controller::on_mouse_down(
+        ge211::events::Mouse_button button,
+        ge211::Posn<int> position)
+{
+    //If the button is active (when game over) AND
+    // mouse pos is within button,
+
+    // Restart game
+
+    //How to reset game?
+}
+
+//Getting user input from typing
 void
 Controller::on_key_up(ge211::Key key)
 {
-    //Double check that '/r' is the Enter key
-    while (key != ge211::Key::code('/r')) {
+    while (key != ge211::Key::code('\r')) {
         player_input = player_input + char(key.code());
     }
-    if (key == ge211::Key::code('/r')) {
-        //check if answer matches any of the answers in the block
+    //If the player hits enter, check if the answer is correct.
+    //If correct, remove block from blocks_
+    if(key == ge211::Key::code('\r')){
+        //Check answer matches, destroy block, add points
+        model_.check_answer(player_input);
+        //Clear player_input
+        player_input = "";
     }
-
-    //std::string input
-        //member of controller
-        //model.handle_input
-            //called when enter is clicked
-
-
-    //on_key, on_key_down, on_key_up
-        //when you type things, it'll return it once
-        //input string: what they're currently typing, which is constantly
-        // modified
-        //when they click enter, trigger handling if it's a solution
-
-        //what if two things have the same solution?
-            //destroy the block closest to the bottom
-
-    // char(key.code());
-    //display everything user types onto the screen
-    //if user presses delete
-    //if user presses enter, set whatever they typed as the player_input
-            //also calls calculate_score()
-
 }
 
-// void
-// Controller::on_mouse_up(ge211::Mouse_button, ge211::Posn<int>)
-// {
-//     model_.launch();
-// }
-
-ge211::Dims<int>
-Controller::initial_window_dimensions() const
-{
-    return view_.initial_window_dimensions();
-}
-
-void
-Controller::load_num_()
-{
-    view_.load_num()
-}
-
-void
-Controller::on_start()
-{
-    return;
-}
-
-std::string
-Controller::get_player_input()
-{
-    return player_input;
-}
